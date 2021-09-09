38b9f57660a6b1feb31ed5a5e56c3d148248c12c
aNd12326 <andersonventosilla0@gmail.com>
2021-09-09 16:46:56 -0500

    14-ternario.js

---
+++ src/bases/14-ternario.js
@@ -0,0 +1,24 @@
+
+
+
+let firstName;
+let lastName = 'tawaynaskp';
+
+
+// console.log(`${firstName || 'No firstName'} ${lastName || 'No lastName'}`);
+
+
+const isActive = true
+
+const message = ( isActive ) 
+                    ? 'Activo' 
+                    : 'Inactivo' //EL === es opcional hacerlo porque esActive es un valor booleano y en teoria es igual al que teniamos antes
+
+
+// if ( isActive ) {
+//     message = 'Activo'
+// } else {
+//     message = 'Inactivo'
+// }
+
+console.log(message);
